b3c886e16755d71ffa6577ac16b802f18c23714a
Guilherme Henrique de Sousa <guilherme.sousa.unespar@gmail.com>
2025-11-11 12:59:59 -0300

    Criação da Janela
    criei

--- jogo_visual.py
+++ jogo_visual.py
@@ -1,3 +1,20 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+
+pygame.init()
+
+largura = 500
+altura = 650
+
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption('Jogo Visual')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+    tela.fill((0, 0, 0))
+    pygame.display.update()
