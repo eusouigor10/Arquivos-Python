0bd89a1c6eba23acd4b0330fe5884f6537236215
Igor Daré <eusouigor10@gmail.com>
2025-11-11 23:07:31 -0300

    Add adicionar_agente() e isPreso(), com a ideia próxima de criar uma configuração inicial aleatória de agentes, sem precisar criar 3 configurações na mão.

--- grafo.py
+++ grafo.py
@@ -6,6 +6,15 @@ class Vertice:
         self.distancia = 0
         self.obstaculo = False
         self.destino = False
+        self.linha = None
+        self.coluna = None
+
+
+class Agente:
+    def __init__(self, grafo, posicao_1, posicao_2, direcao):
+        self.grafo = grafo
+        self.posicoes = (posicao_1, posicao_2)
+        self.direcao = direcao
 
 class Grafo:
 
@@ -13,7 +22,11 @@ class Grafo:
         self.linhas = linhas
         self.colunas = colunas
         self.matriz = []
+        self.agentes = []
         self.cria_matriz()
+        self.criacao_obstaculos()
+        self.destinos()
+        self.preenchimento_matriz()
 
     #criação da matriz
     def cria_matriz(self):
@@ -21,6 +34,8 @@ class Grafo:
             linha = []
             for j in range(self.colunas):
                 v = Vertice()
+                v.linha = i
+                v.coluna = j
                 linha.append(v)
             self.matriz.append(linha)
 
@@ -45,7 +60,7 @@ class Grafo:
         for i in range(self.linhas):
             for j in range(self.colunas):
                 v = self.matriz[i][j]
-                if v.obstaculo != True:
+                if v.obstaculo != True: #se não for obstáculo, verifica os limites da matriz e adiciona na lista de adj.
                     if i - 1 >= 0 and self.matriz[i - 1][j].obstaculo == False:
                         v.lista_adj.append(self.matriz[i - 1][j])
                     if j - 1 >= 0 and self.matriz[i][j - 1].obstaculo == False:
@@ -54,3 +69,40 @@ class Grafo:
                         v.lista_adj.append(self.matriz[i + 1][j])
                     if j + 1 < self.colunas and self.matriz[i][j + 1].obstaculo == False:
                         v.lista_adj.append(self.matriz[i][j + 1])
+    
+    def adicionar_agente(self, i1, j1, i2, j2):
+        #verifica área da matriz
+        if i1 >= 0 and i1 <= 4 and i2 >= 0 and i2 <= 4:
+            if j1 >= 0 and j1 <= 9 and j2 >= 0 and j2 <= 9:
+                #verifica adjacência das duas posições que compõem o agente
+                if self.matriz[i1][j1] in self.matriz[i2][j2].lista_adj:
+                    #verifica se tem por onde andar
+                    if not self.isPreso(i1, j1, i2, j2):
+                        ocupado = False #verifica sobreposição
+                        for agente in self.agentes:
+                            if (i1, j1) in agente.posicoes or (i2, j2) in agente.posicoes:
+                                ocupado = True
+                                break
+                        if not ocupado:
+                            self.agentes.append(Agente(self, (i1, j1), (i2, j2), 'nenhuma'))
+                        
+    def isPreso(self, i1, j1, i2, j2):
+        v1 = self.matriz[i1][j1]
+        v2 = self.matriz[i2][j2]
+        
+        adjacentes = v1.lista_adj + v2.lista_adj #soma as adjacências
+        ocupado = False
+
+        for adjacente in adjacentes: #verifica se algum adjacente tem a mesma posição de algum agente 
+            ocupado = False
+            for agente in self.agentes:
+                if (adjacente.linha, adjacente.coluna) in agente.posicoes:
+                    ocupado = True
+                    break
+            if ocupado == False:
+                return False #se se manteve desocupado, há espaço para andar
+        
+        return True
+        
+                
+            
