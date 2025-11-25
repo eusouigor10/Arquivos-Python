0cac83663545af5ca85a2147e215d482bf36548f
Guilherme Henrique de Sousa <guilherme.sousa.unespar@gmail.com>
2025-11-25 10:35:33 -0300

    Adição da pontuação e mensagens

--- grafo.py
+++ grafo.py
@@ -2,6 +2,7 @@ import random
 import heapq
 import math
 
+
 class Vertice:
     def __init__(self):
         self.lista_adj = []
@@ -19,9 +20,10 @@ class Agente:
         self.direcao = direcao
         self.caminho = None
 
+
 class Grafo:
 
-    def __init__(self, linhas=20,colunas=10):
+    def __init__(self, linhas=20, colunas=10):
         self.linhas = linhas
         self.colunas = colunas
         self.matriz = []
@@ -32,9 +34,9 @@ class Grafo:
         self.preenchimento_matriz()
         self.direcoes = ['esq', 'dir']
         self.lista_linhas_agentes = [0, 1, 2, 3, 4]
-        self.lista_colunas_agentes = [0, 1, 2, 3, 4,5, 6, 7, 8, 9]
+        self.lista_colunas_agentes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    #criação da matriz
+    # criação da matriz
     def cria_matriz(self):
         for i in range(self.linhas):
             linha = []
@@ -45,28 +47,29 @@ class Grafo:
                 linha.append(v)
             self.matriz.append(linha)
 
-    #criação dos obstáculos
+    # criação dos obstáculos
     def criacao_obstaculos(self):
-        vetor = [0,1,2,3,4,5,6,7,8,9]
-        for i in range(6,20):
-            if i % 2 == 0: #apenas em linhas pares
+        vetor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for i in range(6, 20):
+            if i % 2 == 0:  # apenas em linhas pares
                 sorteados = random.sample(vetor, 2)
                 for j in range(10):
                     if j != sorteados[0] and j != sorteados[1]:
                         v = self.matriz[i][j]
                         v.obstaculo = True
-    
-    #definição da linha de destinos
+
+    # definição da linha de destinos
     def destinos(self):
         for j in range(10):
             self.matriz[19][j].destino = True
-    
-    #preenchimento das listas de adjacências
+
+    # preenchimento das listas de adjacências
     def preenchimento_matriz(self):
         for i in range(self.linhas):
             for j in range(self.colunas):
                 v = self.matriz[i][j]
-                if v.obstaculo != True: #se não for obstáculo, verifica os limites da matriz e adiciona na lista de adj.
+                # se não for obstáculo, verifica os limites da matriz e adiciona na lista de adj.
+                if v.obstaculo != True:
                     if i - 1 >= 0 and self.matriz[i - 1][j].obstaculo == False:
                         v.lista_adj.append(self.matriz[i - 1][j])
                     if j - 1 >= 0 and self.matriz[i][j - 1].obstaculo == False:
@@ -75,51 +78,55 @@ class Grafo:
                         v.lista_adj.append(self.matriz[i + 1][j])
                     if j + 1 < self.colunas and self.matriz[i][j + 1].obstaculo == False:
                         v.lista_adj.append(self.matriz[i][j + 1])
-    
+
     def adicionar_agente(self, i1, j1, i2, j2):
         agente_a_ser_adicionado = Agente(self, (i1, j1), (i2, j2), None)
-        #verifica área da matriz
+        # verifica área da matriz
         if i1 >= 0 and i1 <= 4 and i2 >= 0 and i2 <= 4:
             if j1 >= 0 and j1 <= 9 and j2 >= 0 and j2 <= 9:
-                #verifica adjacência das duas posições que compõem o agente
+                # verifica adjacência das duas posições que compõem o agente
                 if self.matriz[i1][j1] in self.matriz[i2][j2].lista_adj:
-                    #verifica se tem por onde andar
+                    # verifica se tem por onde andar
                     if not self.isPreso(agente_a_ser_adicionado):
-                        ocupado = False #verifica sobreposição
+                        ocupado = False  # verifica sobreposição
                         for agente in self.agentes:
                             if (i1, j1) in agente.posicoes or (i2, j2) in agente.posicoes:
                                 ocupado = True
                                 break
-                        if not ocupado: #caso não haja sobreposição
-                            if i2 > i1: #caso esteja na vertical
+                        if not ocupado:  # caso não haja sobreposição
+                            if i2 > i1:  # caso esteja na vertical
                                 direcao = 'cima'
-                            else: #caso esteja na horizontal
+                            else:  # caso esteja na horizontal
                                 direcao = random.choice(self.direcoes)
-                            self.agentes.append(Agente(self, (i1, j1), (i2, j2), direcao))
+                            self.agentes.append(
+                                Agente(self, (i1, j1), (i2, j2), direcao))
                             return True
         return False
-                        
+
     def adicionar_agente_automaticamente(self):
-        cont_sucesso = 0 #contador de vizinhos adicionados
+        cont_sucesso = 0  # contador de vizinhos adicionados
         cont_iteracoes = 0
-        while cont_sucesso < 20 and cont_iteracoes < 1000: #20 é o número máximo de agentes possível
-            i1 = random.choice(self.lista_linhas_agentes) #escolhe uma posição aleatória dentro da área
-            j1 = random.choice(self.lista_colunas_agentes)#de agentes
+        while cont_sucesso <= 40 and cont_iteracoes < 1000:  # 20 é o número máximo de agentes possível
+            # escolhe uma posição aleatória dentro da área
+            i1 = random.choice(self.lista_linhas_agentes)
+            j1 = random.choice(self.lista_colunas_agentes)  # de agentes
             possiveis_vizinhos = []
             if i1 - 1 >= 0:
-                possiveis_vizinhos.append((i1 - 1, j1)) #verifica os limites da matriz e adiciona 
-            if i1 + 1 <= 4:                             #a tupla de possíveis vizinhos
+                # verifica os limites da matriz e adiciona
+                possiveis_vizinhos.append((i1 - 1, j1))
+            if i1 + 1 <= 4:  # a tupla de possíveis vizinhos
                 possiveis_vizinhos.append((i1 + 1, j1))
             if j1 - 1 >= 0:
                 possiveis_vizinhos.append((i1, j1 - 1))
             if j1 + 1 <= 9:
                 possiveis_vizinhos.append((i1, j1 + 1))
-            posicao_escolhida = (random.choice(possiveis_vizinhos)) #escolhe um vizinho aletório
-            i2, j2 = posicao_escolhida                              #e adiciona 
+            # escolhe um vizinho aletório
+            posicao_escolhida = (random.choice(possiveis_vizinhos))
+            i2, j2 = posicao_escolhida  # e adiciona
             cont_iteracoes += 1
             if self.adicionar_agente(i1, j1, i2, j2) == True:
                 cont_sucesso += 1
-    
+
     def isPreso(self, agente):
         frente, tras = agente.posicoes
         i1, j1 = frente
@@ -133,7 +140,7 @@ class Grafo:
         for adj in adjacentes:
             pos = (adj.linha, adj.coluna)
 
-            #não pode se mover para uma posição ocupada por qualquer outro agente
+            # não pode se mover para uma posição ocupada por qualquer outro agente
             ocupado = False
             for outro in self.agentes:
                 if outro is agente:
@@ -143,27 +150,29 @@ class Grafo:
                     break
 
             if not ocupado:
-                return False  #achou um caminho disponível
+                return False  # achou um caminho disponível
 
-        return True  #todos adjacentes bloqueados
-    
-    def reconstruir_caminho(self, destino, predecessores): #o objetivo da função é criar uma lista que representa o caminho percorrido
-        caminho = [] 
-        atual = destino #atual começa sendo o último
+        return True  # todos adjacentes bloqueados
 
-        while atual != None: #enquanto não chega ao fim do dicionário de predecessores
-            caminho.append(atual) #coloca o vértice na lista de caminhos
-            atual = predecessores[atual] #atualiza o vértice para seu predecessor
-        caminho.reverse() #após completar a lista, inverte ela para ficar do primeiro ao último
+    # o objetivo da função é criar uma lista que representa o caminho percorrido
+    def reconstruir_caminho(self, destino, predecessores):
+        caminho = []
+        atual = destino  # atual começa sendo o último
+
+        while atual != None:  # enquanto não chega ao fim do dicionário de predecessores
+            caminho.append(atual)  # coloca o vértice na lista de caminhos
+            # atualiza o vértice para seu predecessor
+            atual = predecessores[atual]
+        caminho.reverse()  # após completar a lista, inverte ela para ficar do primeiro ao último
         return caminho
-    
+
     def dijkstra(self, fonte, agente_atual=None):
         fonte_posicao_i, fonte_posicao_j = fonte
 
         distancias = {}
         predecessores = {}
 
-        #inicialização
+        # inicialização
         for i in range(self.linhas):
             for j in range(self.colunas):
                 distancias[(i, j)] = math.inf
@@ -190,30 +199,32 @@ class Grafo:
                 nova_posicao_j = vizinho.coluna
                 pos = (nova_posicao_i, nova_posicao_j)
 
-                #tratar outros agentes como obstáculos
+                # tratar outros agentes como obstáculos
                 ocupado = False
                 for outro in self.agentes:
                     if outro is agente_atual:
-                        continue  #o próprio agente não conta como obstáculo
+                        continue  # o próprio agente não conta como obstáculo
                     if pos in outro.posicoes:
                         ocupado = True
                         break
 
                 if ocupado:
-                    continue  #ignora essa célula como se fosse obstáculo
+                    continue  # ignora essa célula como se fosse obstáculo
 
                 nova_distancia = distancia_atual + 1
 
                 # Relaxamento
                 if nova_distancia < distancias[(nova_posicao_i, nova_posicao_j)]:
-                    distancias[(nova_posicao_i, nova_posicao_j)] = nova_distancia
+                    distancias[(nova_posicao_i, nova_posicao_j)
+                               ] = nova_distancia
                     predecessores[(nova_posicao_i, nova_posicao_j)] = (i, j)
-                    heapq.heappush(heap, (nova_distancia, (nova_posicao_i, nova_posicao_j)))
+                    heapq.heappush(
+                        heap, (nova_distancia, (nova_posicao_i, nova_posicao_j)))
 
         return None
 
-        
-    def caminho_agente(self, agente): #essa função retorna o caminho mais curto entre os dois das duas posições do agente
+    # essa função retorna o caminho mais curto entre os dois das duas posições do agente
+    def caminho_agente(self, agente):
         p1 = agente.posicoes[0]
         p2 = agente.posicoes[1]
         caminho1 = self.dijkstra(p1, agente_atual=agente)
@@ -222,7 +233,7 @@ class Grafo:
             return caminho2
         if caminho2 == None:
             return caminho1
-        
+
         if len(caminho1) <= len(caminho2):
             return caminho1, p1, p2
         else:
@@ -230,32 +241,29 @@ class Grafo:
 
     def movimentacao_agente(self, agente):
 
-        #se ainda não tem caminho calculado (primeira interação de cada agente), calcula uma vez
+        # se ainda não tem caminho calculado (primeira interação de cada agente), calcula uma vez
         if agente.caminho is None or len(agente.caminho) <= 1:
             caminho, posicao_frente, posicao_tras = self.caminho_agente(agente)
             agente.caminho = caminho
 
         caminho = agente.caminho
 
-        #se o caminho acabou
+        # se o caminho acabou
         if len(caminho) <= 1 or caminho is None:
             return False
 
-        #remove posição atual (frente) e segue para a próxima
+        # remove posição atual (frente) e segue para a próxima
         caminho.pop(0)
 
-        nova_frente = caminho[0] 
+        nova_frente = caminho[0]
         nova_tras = agente.posicoes[0]
 
-        agente.posicoes = (nova_frente, nova_tras) #atualiza as posições do agente (sensação de movimento)
-        
+        # atualiza as posições do agente (sensação de movimento)
+        agente.posicoes = (nova_frente, nova_tras)
+
         # depois de mover este agente, atualizar caminhos dos outros
         for outro in self.agentes:
             if outro is not agente:
                 outro.caminho = None
 
         return True
-
-
-
-            
--- jogo_visual.py
+++ jogo_visual.py
@@ -11,7 +11,7 @@ class JogoVisual:
 
         self.grafo = grafo
         self.largura = largura
-        self.altura = altu
+        self.altura = altura
 
         self.CELULA_LARGURA = largura // grafo.colunas
         self.CELULA_ALTURA = altura // grafo.linhas
@@ -29,9 +29,15 @@ class JogoVisual:
         self.carregar_imagens()
 
         self.agente_selecionado = None
-        self.caminho_verde = []   # novo
+        self.caminho_verde = []
 
-    def carregar_imagens(self):  # Carregar imagens, se existirem na pasta do projeto
+        self.pontos = 0
+        self.fonte = pygame.font.SysFont("arial", 32, True)
+
+        self.mensagem_temp = ""
+        self.mensagem_tempo = 0
+
+    def carregar_imagens(self):
         dir = os.path.dirname(os.path.abspath(__file__))
 
         def load(name):
@@ -46,7 +52,7 @@ class JogoVisual:
         self.img_dest = load("destino.png")
         self.img_carro = load("agente.png")
 
-    def desenhar_fundo(self):  # Desenhar fundo ou preencher branco, se não houver imagem
+    def desenhar_fundo(self):
         if self.img_fundo:
             fundo = pygame.transform.scale(
                 self.img_fundo, (self.largura, self.altura))
@@ -54,11 +60,9 @@ class JogoVisual:
         else:
             self.tela.fill((255, 255, 255))
 
-    # Desenhar área de saída ou preencher branco, se não houver imagem
     def desenhar_saida(self):
         y = 0
         h = self.CELULA_ALTURA
-
         if self.img_dest:
             dst = pygame.transform.scale(self.img_dest, (self.largura, h))
             self.tela.blit(dst, (0, 0))
@@ -66,23 +70,18 @@ class JogoVisual:
             pygame.draw.rect(self.tela, (255, 255, 255),
                              (0, y, self.largura, h))
 
-    # Desenhar área de início (transparente azul claro)
     def desenhar_area_inicio(self):
         y = self.inv(4) * self.CELULA_ALTURA
         h = 5 * self.CELULA_ALTURA
-
         s = pygame.Surface((self.largura, h), pygame.SRCALPHA)
         s.fill((150, 200, 255, 90))
         self.tela.blit(s, (0, y))
 
-    # Desenhar obstáculos (cones) ou preencher vermelho, se não houver imagem
     def desenhar_obstaculos(self):
         for i in range(self.grafo.linhas):
             for j in range(self.grafo.colunas):
-
                 v = self.grafo.matriz[i][j]
                 if v.obstaculo:
-
                     iv = self.inv(i)
                     x = j * self.CELULA_LARGURA
                     y = iv * self.CELULA_ALTURA
@@ -95,7 +94,7 @@ class JogoVisual:
                         pygame.draw.rect(self.tela, (200, 0, 0),
                                          (x, y, self.CELULA_LARGURA, self.CELULA_ALTURA))
 
-    def desenhar_caminho_verde(self):  # Desenhar caminho em verde transparente
+    def desenhar_caminho_verde(self):
         if not self.caminho_verde:
             return
 
@@ -109,10 +108,8 @@ class JogoVisual:
             s.fill(self.VERDE_CAMINHO)
             self.tela.blit(s, (x, y))
 
-    # Desenhar agentes (carros) ou preencher azul, se não houver imagem
     def desenhar_agentes(self):
         for agente in self.grafo.agentes:
-
             (i1, j1), (i2, j2) = agente.posicoes
 
             iv1 = self.inv(i1)
@@ -128,18 +125,15 @@ class JogoVisual:
             w = (max_j - min_j + 1) * self.CELULA_LARGURA
             h = (max_i - min_i + 1) * self.CELULA_ALTURA
 
-            if self.img_carro:  # Desenhar carro com imagem, rotacionando se necessário
+            if self.img_carro:
                 img = self.img_carro
                 if h > w:
                     img = pygame.transform.rotate(img, -90)
-
                 carro = pygame.transform.scale(img, (w, h))
                 self.tela.blit(carro, (x, y))
-
             else:
                 pygame.draw.rect(self.tela, (0, 0, 255), (x, y, w, h))
 
-    # Detectar clique em um agente, retornando o agente clicado
     def detectar_clique(self, pos):
         mx, my = pos
 
@@ -160,41 +154,58 @@ class JogoVisual:
             h = (max_i - min_i + 1) * self.CELULA_ALTURA
 
             rect = pygame.Rect(x, y, w, h)
-
             if rect.collidepoint((mx, my)):
                 return agente
 
         return None
 
-    # Animar movimento do agente ao longo do caminho encontrado
     def animar_movimento(self, agente):
-        caminho, _, _ = self.grafo.caminho_agente(agente)
+        caminho, _, chegou = self.grafo.caminho_agente(agente)
 
         if not caminho:
             print("Sem caminho possível!")
             return
 
         for passo in caminho:
-
             agente.posicoes = (passo, agente.posicoes[0])
-
             self.atualizar_tela()
-            pygame.time.delay(120)
+            pygame.time.delay(80)  # delay para visualização
 
         if agente in self.grafo.agentes:
             self.grafo.agentes.remove(agente)
 
-    def atualizar_tela(self):  # Atualizar tela desenhando todos os elementos
+        if chegou:
+            self.pontos += 2
+            self.mensagem_temp = "Chegou ao destino! +2 pontos"
+            self.mensagem_tempo = 120
+
+    def desenhar_pontuacao(self):
+        txt = self.fonte.render(f"Pontos: {self.pontos}", True, (66, 245, 239))
+        y_pontos = self.altura - txt.get_height() - 10
+
+        self.tela.blit(txt, (10, y_pontos))
+
+        if self.mensagem_temp:
+            txt2 = self.fonte.render(self.mensagem_temp, True, (66, 245, 239))
+            self.tela.blit(txt2, (self.largura // 2 -
+                           txt2.get_width() // 2, 40))
+
+            self.mensagem_tempo -= 1
+            if self.mensagem_tempo <= 0:
+                self.mensagem_temp = ""
+
+    def atualizar_tela(self):
         self.desenhar_fundo()
         self.desenhar_saida()
         self.desenhar_area_inicio()
         self.desenhar_obstaculos()
         self.desenhar_caminho_verde()
         self.desenhar_agentes()
+        self.desenhar_pontuacao()
 
         pygame.display.update()
 
-    def executar(self):  # Loop principal do jogo, tratando eventos e atualizando a tela
+    def executar(self):
 
         while True:
             for e in pygame.event.get():
@@ -202,44 +213,50 @@ class JogoVisual:
                     pygame.quit()
                     return
 
-                # ...
-            if e.type == MOUSEBUTTONDOWN and e.button == 1:
-                agente = self.detectar_clique(e.pos)
+                if e.type == MOUSEBUTTONDOWN and e.button == 1:
+                    agente = self.detectar_clique(e.pos)
 
-                if agente:
-                    # 1. Tenta calcular o caminho para o agente clicado
-                    resultado = None
-                    if not self.grafo.isPreso(agente):
-                        resultado = self.grafo.caminho_agente(agente)
+                    if agente:
+                        # tenta calcular caminho
+                        resultado = None
+                        if not self.grafo.isPreso(agente):
+                            resultado = self.grafo.caminho_agente(agente)
 
-                    caminho = resultado[0] if resultado is not None else None
+                        caminho = resultado[0] if resultado else None
 
-                    # 2. Lógica de clique
-                    if self.agente_selecionado == agente:
+                        # SE CLICOU NO MESMO AGENTE
+                        if self.agente_selecionado == agente:
 
-                        if caminho:  # Se houver caminho calculado anteriormente
-                            self.animar_movimento(agente)  # Passa o caminho
-                            self.caminho_verde = []
-                            self.agente_selecionado = None
+                            if caminho:
+                                self.animar_movimento(agente)
+                                self.caminho_verde = []
+                                self.agente_selecionado = None
+                            else:
+                                # PERDE PONTOS
+                                self.pontos -= 3
+                                self.mensagem_temp = "Carro preso! -3 pontos"
+                                self.mensagem_tempo = 90
+
+                                print("Agente preso!")
+                                self.caminho_verde = []
+
                         else:
-                            print("Agente preso ou sem caminho possível!")
-                            self.caminho_verde = []
-                    else:
-                        # Seleciona o agente e mostra o caminho (se houver)
-                        self.agente_selecionado = agente
-                        if caminho:
-                            self.caminho_verde = caminho
-                        else:
-                            self.caminho_verde = []
-                            print("Agente preso ou sem caminho possível!")
-                            # Não precisa do 'return' aqui, a tela será atualizada
-# ...
+                            # Seleciona o agente e mostra caminho
+                            self.agente_selecionado = agente
+                            if caminho:
+                                self.caminho_verde = caminho
+                            else:
+                                self.pontos -= 3
+                                self.mensagem_temp = "Carro preso! -3 pontos"
+                                self.mensagem_tempo = 90
+
+                                self.caminho_verde = []
 
             self.atualizar_tela()
             self.clock.tick(60)
 
 
-if __name__ == "__main__":  # Executa o jogo visual se o arquivo for executado diretamente
+if __name__ == "__main__":
     from grafo import Grafo
 
     grafo = Grafo()
